70af3d757876db3c133eee93e1b3e578f4dd2c0d
Dany Poroshan <dany.poroshan.95@gmail.com>
2023-04-09 21:50:06 +0300

    refactor

--- src/plotter.py
+++ src/plotter.py
@@ -1,10 +1,10 @@
-class Plotter: # todo: implement
+class Plotter:  # todo: implement
     pass
 
 # Example of plots
-    # plt.figure()
-    # plt.subplot(1, 2, 1)  # last param is the subplot number
-    # plt.imshow(a.grayscale_image, cmap='gray', vmin=0, vmax=255)
-    # plt.subplot(1, 2, 2)
-    # plt.imshow(a.color_image, vmin=0, vmax=255)
-    # plt.show()
+# plt.figure()
+# plt.subplot(1, 2, 1)  # last param is the subplot number
+# plt.imshow(a.grayscale_image, cmap='gray', vmin=0, vmax=255)
+# plt.subplot(1, 2, 2)
+# plt.imshow(a.color_image, vmin=0, vmax=255)
+# plt.show()
