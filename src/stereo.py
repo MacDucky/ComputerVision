7d598cff2d97488266d21fb88024b76b0826881a
NirSeg <99889837+NirSeg@users.noreply.github.com>
2023-05-19 15:43:45 +0300

    added a proper init stereo

--- src/stereo.py
+++ src/stereo.py
@@ -2,18 +2,19 @@ import numpy as np
 from loader import ImageLoader
 from camera import Camera
 class Stereo:
-    def __init__(self, iml : ImageLoader, imr : ImageLoader, caml : Camera, camr: Camera):
-        self.iml = iml
-        self.imr = imr
-        self.caml = caml
-        self.camr = camr
+    def __init__(self, image_left : ImageLoader, image_right : ImageLoader, camera_left : Camera, camera_right: Camera):
+        self.image_left = image_left
+        self.image_right = image_right
+        self.camera_left = camera_left
+        self.camera_right = camera_right
         # Disparities Maps
-        self.displ = None
-        self.dispr = None
+        self.disparity_left = None
+        self.disparity_right = None
         # Depth Maps
-        self.depl = None
-        self.depr = None
-        # Reprojection Matrix
-        self.reprojection = None
+        self.depth_left = None
+        self.depth_right = None
+        # Back Projection Matrix from camera 1 (all the pixels represented in camera axis 1)
+        self.back_projection = None
 
+    def creat_disps(self)
 
