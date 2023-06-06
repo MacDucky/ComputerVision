a9feaff6e024135df948370c6507270d73b1ff5c
NirSeg <99889837+NirSeg@users.noreply.github.com>
2023-06-06 18:12:28 +0300

    disparity working

--- src/stereo.py
+++ src/stereo.py
@@ -1,10 +1,12 @@
 import os
 import cv2
 import numpy as np
+from plotter import compare_images
 from loader import ImageLoader
 from camera import Camera
 from numpy import ndarray
 from timeit import default_timer as perf_counter
+
 
 
 class Stereo:
@@ -36,22 +38,17 @@ class Stereo:
         zero_one = np.full(sliding_window.shape, 1)
         one_zero = np.full(sliding_window.shape, 2)
         zero_zero = np.full(sliding_window.shape, 0)
-        neg_neg = np.full(sliding_window.shape, 3)
 
         # apply census mapping on sliding windows
-        census_values = np.where(sliding_window == -1, neg_neg,
-                                 np.where(sliding_window > (center_value + rho), one_zero,
-                                          np.where(sliding_window < (center_value - rho), zero_one,
-                                                   zero_zero)))
+        census_values = np.where(sliding_window > (center_value + rho), one_zero,
+                                 np.where(sliding_window < (center_value - rho), zero_one,
+                                          zero_zero))
 
         # unpack decimals to bits
         census_bits = np.unpackbits(census_values.astype(np.uint8), axis=-1)
         census_bits = census_bits.flatten().reshape((*sliding_window.shape, 8))
         # reduce to 2 bit representation
-        census_bits = np.array(census_bits)[:, :, :, :, -2:]
-
-        # map [1,1] to [-1,-1]
-        final_census = np.where((census_bits == [1, 1]).all(axis=-1, keepdims=True), [-1, -1], census_bits)
+        final_census = np.array(census_bits)[:, :, :, :, -2:]
 
         # remove center value at each window
         remove_center_mask = np.ones_like(final_census, dtype=bool)
@@ -74,21 +71,14 @@ class Stereo:
             A census map of the image
         """
         padded_height, padded_width = (window_size[0] - 1) // 2, (window_size[1] - 1) // 2
-        padded_image = np.pad(image.astype(int), (padded_height, padded_width), constant_values=-1)
+        padded_image = np.pad(image.astype(int), (padded_height, padded_width), constant_values=0)
         sliding_window = np.lib.stride_tricks.sliding_window_view(padded_image, window_size)
         census_map = Stereo.__create_census(sliding_window, rho)
         return census_map
 
     @staticmethod
-    def compare_census(census1: ndarray, census2: ndarray):
-        if census1.all(-np.ones_like(census1), keepdims=True) or census1.all(-np.ones_like(census2), keepdims=True):
-            return 255
-        res = np.sum(np.clip(census1 + census2, a_min=0, a_max=2) % 2)
-        return res
-
-    @staticmethod
     def winner_takes_all(census_image_src: ndarray, census_image_candidate: ndarray, is_left_im: bool,
-                         arm_length: int = 30):
+                         arm_length: int, max_cost: int):
         """
         Returns the cost image of the source image given the census source image and the other census image.
 
@@ -97,151 +87,148 @@ class Stereo:
             census_image_candidate: the other census image.
             is_left_im(bool): True if the source image is the left image.
             arm_length(int): the size of the cost array for each pixel.
+            max_cost: maximum cost of a census
         Returns:
-            return the cost image
+            return the disparity
         """
         height, width, _ = census_image_src.shape
-        padded_image = -np.ones((height, width + arm_length - 1, census_image_src.shape[-1]))
-        if is_left_im:
-            padded_image[:, arm_length:] = census_image_candidate[:, :-1]
-        else:
-            padded_image[:, :-arm_length] = census_image_candidate[:, :-1]
+        disparity = np.full((height, width, 4), max_cost)
+        for d in range(1, arm_length + 1):
+            x = np.arange(width)
 
-        cost_windows = np.lib.stride_tricks.sliding_window_view(padded_image, (arm_length, census_image_src.shape[-1]),
-                                                                axis=(1, 2))
-        cost_windows = cost_windows.reshape((*census_image_src.shape[:2], arm_length, census_image_src.shape[-1]))
+            x_right = x[:] - d if is_left_im else x[:] + d
+            invalid_indices = (x_right < 0) | (x_right >= width)
+            x_right = np.where(invalid_indices, 0, x_right)
 
-        src_window = np.full_like()
-        disparity = np.zeros((height, width))
-        delta = -1
-        if not is_left_im:
-            delta = 1
-        for i in range(height):
-            for j in range(width):
-                cost = (
-                    Stereo.compare_census(census_image_src[i, j], census_image_candidate[i, j + delta * (k + 1)]) for k
-                    in range(arm_length))
-                cost = np.array(list(cost))
-                # Get the indices that would sort the array
-                sorted_indices = np.argsort(cost)
-                # Get the indices of the least 4 values
-                least_indices = sorted_indices[:4]
+            cost_volume = np.where(invalid_indices, np.inf, np.sum(
+                np.bitwise_xor(census_image_src[:, :, :], census_image_candidate[:, x_right, :]), axis=2))
 
-                # Sort the least indices to maintain stability
-                min_indices = np.sort(least_indices)
+            cost_volume = cv2.GaussianBlur(cost_volume, (0, 0), 1)
 
-                values = cost[min_indices]
-                if values.min() != values.max():
-                    values = values[:-1]
-                    if values.min() == values.max():
-                        disparity[i, j] = min_indices[1]
-                    else:
-                        disparity[i, j] = min_indices[0]
+            # mask for: new minimum is found
+            min_mask = cost_volume < disparity[..., 0]
+
+            # mask for: another minimum is found
+            equal_mask = np.logical_and(cost_volume == disparity[..., 0], np.logical_not(min_mask))
+
+            # mask for: another minimum is found and it is the third
+            triple_mask = disparity[..., 2] == 1
+
+            # mask for: another minimum is found and we already have three or more
+            more_than_triple_mask = disparity[..., 2] >= 2
+
+            disparity[min_mask, 0] = cost_volume[min_mask]
+            disparity[min_mask, 1] = d
+            disparity[min_mask, 2] = 0
+            disparity[min_mask, 3] = d
+
+            disparity[equal_mask & triple_mask, 1] = disparity[equal_mask & triple_mask, 3]
+
+            disparity[equal_mask & more_than_triple_mask, 1] = 0
+
+            disparity[equal_mask, 2] += 1
+            disparity[equal_mask, 3] = d
+
+        disparity = disparity[..., 1].astype(int)
         return disparity
 
-        # min_cost = min(cost)
-        # min_indices = [index for index, value in enumerate(cost) if value == min_cost]
-        # len_cost_min = len(min_indices)
-        # if len_cost_min > 0:
-        #     if len_cost_min == 1:
-        #         disparity[i, j] = min_indices[0] + 1
-        #     elif len_cost_min == 2 and min_indices[1] - min_indices[0] == 1:
-        #         disparity[i, j] = min_indices[0] + 1
-        #     elif len_cost_min == 3 and min_indices[2] - min_indices[0] == 2:
-        #         disparity[i, j] = min_indices[1] + 1
-        # print(i*height+j)
+    @staticmethod
+    def consistency_test(src_disparity: ndarray, dest_disparity: ndarray, is_left_img: int):
+        """
+        Returns an improved disparity
 
-    # @staticmethod
-    # def winner_takes_all(cost_image: ndarray):
-    #     """
-    #     Returns a disparity map given the cost image.
-    #
-    #     Parameters:
-    #         cost_image(ndarray): image of all the candidates cost of a specific pixel.
-    #     Returns:
-    #          A disparity map (not final).
-    #     """
-    #     height, width = cost_image.shape
-    #     disparity = np.zeros(shape=(height, width))
-    #     for i in range(height):
-    #         for j in range(width):
-    #             min_cost = min(cost_image[i, j])
-    #             min_indices = [index for index, value in enumerate(cost_image[i, j]) if value == min_cost]
-    #             len_cost_min = len(min_indices)
-    #             if len_cost_min > 0:
-    #                 if len_cost_min == 1:
-    #                     disparity[i, j] = min_indices[0] + 1
-    #                 elif len_cost_min == 2 and min_indices[1] - min_indices[0] == 1:
-    #                     disparity[i, j] = min_indices[0] + 1
-    #                 elif len_cost_min == 3 and min_indices[2] - min_indices[0] == 2:
-    #                     disparity[i, j] = min_indices[1] + 1
-    #     return disparity
+        Parameters:
+            src_disparity: source disparity, the disparity we want to improve
+            dest_disparity: destination disparity, the disparity we use to improve the source disparity
+            is_left_img: is the source image is the left image?
+        Returns:
+             source disparity
+        """
+        height, width = src_disparity.shape
+        consistency_map = np.zeros((height, width), dtype=np.float32)
+        x_coords, y_coords = np.meshgrid(np.arange(width), np.arange(height))
 
-    def create_disparities(self, window_size: tuple[int, int], rho: int):
+        r_x_coords = x_coords - (2 * is_left_img - 1) * src_disparity
+        valid_coords = np.logical_and(r_x_coords >= 0, r_x_coords < width)
+
+        valid_left_disparity = src_disparity[valid_coords]
+        valid_right_disparity = dest_disparity[y_coords[valid_coords], r_x_coords[valid_coords].astype(int)]
+
+        consistency_mask = valid_left_disparity == valid_right_disparity
+        consistency_map[y_coords[valid_coords][consistency_mask], x_coords[valid_coords][consistency_mask]] = \
+            valid_left_disparity[consistency_mask]
+
+        window_size = 25
+        window_half = window_size // 2
+
+        for y in range(height):
+            for x in range(width):
+                if consistency_map[y, x] == 0:
+                    ymin = max(0, y - window_half)
+                    ymax = min(height, y + window_half + 1)
+                    xmin = max(0, x - window_half)
+                    xmax = min(width, x + window_half + 1)
+
+                    neighbors = consistency_map[ymin:ymax, xmin:xmax]
+                    unique, counts = np.unique(neighbors, return_counts=True)
+                    most_common_value = unique[np.argmax(counts)]
+
+                    consistency_map[y, x] = most_common_value
+
+        return consistency_map
+
+    def calc_disparity_maps(self, window_size: tuple[int, int], rho: int):
         """
         Create disparity maps to both left & right images, then apply a winner takes it all + postprocessing.
         """
+        start_time_disparity = perf_counter()
         # Census map creation
-        from multiprocessing import Pool
-        start_time = perf_counter()
-        # with Pool(processes=2) as pool:
-        #     left_res = pool.apply_async(self.create_census_map, (self.image_left.grayscale_img, window_size, rho))
-        #     right_res = pool.apply_async(self.create_census_map, (self.image_right.grayscale_img, window_size, rho))
-        #     census_map_left = left_res.get()
-        #     census_map_right = right_res.get()
 
+        start_time_census_left = perf_counter()
         census_map_left = self.create_census_map(self.image_left.grayscale_img, window_size, rho)
+        total_time_census_left = perf_counter() - start_time_census_left
+        print(f'Total time taken during left census map creation: {total_time_census_left}')
+
+        start_time_census_right = perf_counter()
         census_map_right = self.create_census_map(self.image_right.grayscale_img, window_size, rho)
-        total_time = perf_counter() - start_time
-        print(f'Total time taken during census map creation: {total_time}')
+        total_time_census_left = perf_counter() - start_time_census_right
+        print(f'Total time taken during right census map creation: {total_time_census_left}')
+
+        max_cost = 2 * (window_size[0] * window_size[1] - 1)
 
         # the cost aggregations for every pixel in the left image
-        left_disparity = self.winner_takes_all(census_map_left, census_map_right, True)
-        right_disparity = self.winner_takes_all(census_map_right, census_map_left, False)
+        start_time_census_right = perf_counter()
+        left_disparity = self.winner_takes_all(census_map_left, census_map_right, True, self.max_disparity,
+                                               max_cost=max_cost)
+        total_time_census_left = perf_counter() - start_time_census_right
+        print(f'Total time taken during winner takes all technique for the left disparity: {total_time_census_left}')
 
-        # Threshold the disparity maps
-        # todo: make a threshold using max disparity
+        start_time_wta_right = perf_counter()
+        right_disparity = self.winner_takes_all(census_map_right, census_map_left, False, self.max_disparity,
+                                                max_cost=max_cost)
+        total_time_wta_right = perf_counter() - start_time_wta_right
+        print(f'Total time taken during winner takes all technique for the right disparity: {total_time_wta_right}')
 
-    def calc_depth_maps(self):
-        """
-        Calculates depth maps for the cameras.
-        """
-        left_cam_origin = self.camera_left.origin[:-1]
-        right_cam_origin = self.camera_right.origin[:-1]
-        baseline_len = np.linalg.norm(left_cam_origin - right_cam_origin)
-        focal_len = self.camera_left.focal_len  # assumes same intrinsics for both cameras
-        similarity_ratio = baseline_len * focal_len
-        if self.depth_left is None:
-            self.depth_left = np.divide(similarity_ratio, self.disparity_left, where=self.disparity_left != 0)
-            self.depth_left = np.nan_to_num(self.depth_left)
+        start_time_consistency_left = perf_counter()
+        self.disparity_left = Stereo.consistency_test(left_disparity, right_disparity, True)
+        total_time_consistency_left = perf_counter() - start_time_consistency_left
+        print(
+            f'Total time taken during consistency test technique for the left disparity: {total_time_consistency_left}')
 
-        if self.depth_right is None:
-            self.depth_right = np.divide(similarity_ratio, self.disparity_right, where=self.disparity_right != 0)
-            self.depth_right = np.nan_to_num(self.depth_right)
+        start_time_consistency_right = perf_counter()
+        self.disparity_right = Stereo.consistency_test(right_disparity, left_disparity, False)
+        total_time_consistency_right = perf_counter() - start_time_consistency_right
+        print(
+            f'Total time taken during consistency test technique for the right disparity: {total_time_consistency_right}')
 
-    def calc_back_projection(self):
-        """ Calculates the back projection for the left camera in the setup. """
-        _, i_intrinsics = cv2.invert(self.camera_left.intrinsic_transform, flags=cv2.DECOMP_SVD)
-        height, width = self.depth_left.shape
+        total_time_disparity = perf_counter() - start_time_disparity
+        print(f'Total time taken during disparity creation: {total_time_disparity}')
 
-        # Creating a pixel coordinate matrix
-        x = np.linspace(0, width - 1, width).astype(int)
-        y = np.linspace(0, height - 1, height).astype(int)
-        [x, y] = np.meshgrid(x, y)
-        pixel_coordinates = np.vstack(
-            (x.flatten(), y.flatten(), np.ones_like(x.flatten()) * self.camera_left.focal_len))
-
-        # Scale each point on the line from camera center and the pixel coordinate on the image plane(as camera coords),
-        # to its original z-value
-        self.__back_projection = i_intrinsics @ pixel_coordinates * self.depth_left.flatten()
-
-    @property
-    def back_projection(self):
-        if self.depth_left is None:
-            self.calc_depth_maps()
-        if self.__back_projection is None:
-            self.calc_back_projection()
-        return self.__back_projection.copy()
+        # disparity_left_im = cv2.normalize(self.disparity_left, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX,
+        #                                   dtype=cv2.CV_8U)
+        # disparity_right_im = cv2.normalize(self.disparity_right, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX,
+        #                                    dtype=cv2.CV_8U)
+        # compare_images(disparity_left_im, disparity_right_im)
 
 
 if __name__ == '__main__':
@@ -251,5 +238,5 @@ if __name__ == '__main__':
     right_im_path = os.path.abspath(r'..\assignment_2\example\im_right.jpg')
     left_image = ImageLoader(left_im_path)
     right_image = ImageLoader(right_im_path)
-    stereo = Stereo(left_image, right_image, left_camera, right_camera, 255)
-    stereo.create_disparities(window_size=tuple((5, 5)), rho=2)
+    stereo = Stereo(left_image, right_image, left_camera, right_camera, 77)
+    stereo.calc_disparity_maps(window_size=tuple((9, 9)), rho=1)
